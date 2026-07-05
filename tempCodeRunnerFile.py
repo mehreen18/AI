da8f4d517e261f411a2392ca44cfcbbd2b90c877
mehreen18 <m14743547@gmail.com>
2026-07-05 12:44:37 +0500

    3

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,42 @@
-## Relue
+#  neural network
 import torch
 import torch.nn as nn
-model = nn.Sequential(
-    nn.Linear(4,8),
-    nn.ReLU(),
-    nn.Linear(8,2)
-)
+
+class MyNeuralNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.network = nn.Sequential(
+
+            nn.Linear(4, 16), 
+            nn.ReLU(),
+
+            nn.Linear(16, 32), 
+            nn.ReLU(),
+
+            nn.Linear(32, 64), 
+            nn.ReLU(),
+
+            nn.Linear(64, 32),
+            nn.ReLU(),
+
+            nn.Linear(32, 16),  
+            nn.ReLU(),
+
+            nn.Linear(16, 2)   
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+model = MyNeuralNetwork()
 
 print(model)
+
+x=torch.tensor([[1.23,2.34,3.45,5.0]])
+
+output=model(x)
+print(x)
+print(output)
